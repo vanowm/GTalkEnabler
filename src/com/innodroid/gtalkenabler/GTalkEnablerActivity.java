282a0fe2f38cbed1fec88ed1d6dbeb83a4f37010
Greg Ennis <admin@innodroid.com>
2011-07-30 16:39:40 -0400

    Layout of main form

--- src/com/innodroid/gtalkenabler/GTalkEnablerActivity.java
+++ src/com/innodroid/gtalkenabler/GTalkEnablerActivity.java
@@ -2,12 +2,28 @@ package com.innodroid.gtalkenabler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class GTalkEnablerActivity extends Activity {
-    /** Called when the activity is first created. */
+	
+	private CheckBox mEnableCheckBox;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        mEnableCheckBox = (CheckBox)findViewById(R.id.allow_3g_chat);
+        
+        ((Button)findViewById(R.id.save_button)).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}        	
+        });
     }
 }
+
