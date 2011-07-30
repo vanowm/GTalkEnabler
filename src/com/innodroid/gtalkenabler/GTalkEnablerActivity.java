9ce80eb5cfaf11a671bbdb66ebd4e0c2d330621d
Greg Ennis <admin@innodroid.com>
2011-07-30 11:14:10 -0400

    Initial project creation

---
+++ src/com/innodroid/gtalkenabler/GTalkEnablerActivity.java
@@ -0,0 +1,13 @@
+package com.innodroid.gtalkenabler;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GTalkEnablerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
